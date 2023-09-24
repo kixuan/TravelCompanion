f6aef7a1fa42277d3dd0104cd31b3cd3299d0e3b
kixuan <1752845542@qq.com>
2023-09-24 13:36:55 +0800

    feat：Blog点赞排行

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -50,4 +50,9 @@ public class BlogController {
         return blogService.queryBlogById(id);
     }
 
+    @GetMapping("/likes/{id}")
+    public Result queryBlogLikes(@PathVariable("id") Long id) {
+        return blogService.queryBlogLikes(id);
+    }
+
 }
