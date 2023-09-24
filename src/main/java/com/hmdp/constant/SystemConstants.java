58c40b3b5bc81daf25f8bf63c72a747788b89e38
kixuan <1752845542@qq.com>
2023-09-24 09:26:55 +0800

    feat：发布、查看探店笔记

--- src/main/java/com/hmdp/constant/SystemConstants.java
+++ src/main/java/com/hmdp/constant/SystemConstants.java
@@ -1,7 +1,7 @@
 package com.hmdp.constant;
 
 public class SystemConstants {
-    public static final String IMAGE_UPLOAD_DIR = "D:\\lesson\\nginx-1.18.0\\html\\hmdp\\imgs\\";
+    public static final String IMAGE_UPLOAD_DIR = "E:\\Project\\Java\\hmdpAll\\nginx-1.18.0\\html\\hmdp\\imgs";
     public static final String USER_NICK_NAME_PREFIX = "user_";
 
     public static final int DEFAULT_PAGE_SIZE = 5;
--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    Result saveBlog(Blog blog);
+
+    Result queryMyBlog(Integer current);
+
+    Result queryHotBlog(Integer current);
+
+    Result likeBlog(Long id);
 }
