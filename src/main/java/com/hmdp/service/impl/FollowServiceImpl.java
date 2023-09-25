add08982d165e614a851133a88844d905c9676e1
kixuan <1752845542@qq.com>
2023-09-25 09:04:32 +0800

    feat:好友关注：关注和取消关注

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,21 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author kixuan
@@ -16,5 +23,42 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 判断是否关注
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1. 获取当前登录用户
+        long userId = UserHolder.getUser().getId();
+
+        // 2. 从数据库中查是否有关注数据
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        return Result.ok(count > 0);
+    }
+
+    /**
+     * 进行关注/取关操作
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 1. 获取当前登录用户
+        long userId = UserHolder.getUser().getId();
+
+        // 2. 判断是否关注
+        if (isFollow) {
+            // 未关注，进行关注操作
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            follow.setCreateTime(LocalDateTime.now());
+            save(follow);
+        } else {
+            // 关注，进行取关操作
+            remove(new QueryWrapper<Follow>().eq("user_id", userId).eq("follow_user_id", followUserId));
+        }
+        return Result.ok();
+    }
 }
